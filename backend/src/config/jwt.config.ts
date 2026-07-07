b6881f9eac1fb1a4c1a8578cd810bfc9c986f706
rizkiramadhanx <rizkijitu77@gmail.com>
2026-07-07 23:44:51 +0700

    coomi

--- backend/src/config/jwt.config.ts
+++ backend/src/config/jwt.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => ({
   jwt: {
     secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN || '3600s',
+    expiresIn: process.env.JWT_EXPIRES_IN || '30d',
   },
   refresh: {
     secret: process.env.JWT_SECRET_REFRESH,
